cf81016ffc463803dacc4fe7fd9006cd1b7099c0
Parneet Saxena <parneetsaxena@gmail.com>
2026-03-04 17:07:49 +0530

    Array Problems

---
+++ src/arrays/SumOfTriplets.java
@@ -0,0 +1,22 @@
+package arrays;
+
+public class SumOfTriplets {
+    static int sumOfTriplets(int[] arr,int target){
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            for(int j = i+1;j<arr.length;j++){
+                for(int k = j+1;k<arr.length;k++){
+                    if(arr[i]+arr[j]+arr[k]==target){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,30,40,60,80,50,100};
+        System.out.println(sumOfTriplets(arr,150));
+    }
+}
